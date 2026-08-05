0f98729ed6312d09fd2c582f807c126fbd5e9fed
Prashant Chouhan <pchouhan122@gmail.com>
2026-08-05 12:43:39 +0530

    2 Question max array number and second max from array.

--- array.js
+++ array.js
@@ -43,10 +43,54 @@ let arr2 = new Array(3);
 // console.log(arr)
 
 // Q 43 sum array array's elements 
+let arr = [10, 20, 30, 40, 50]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+// }
+// console.log(`Sum of array's elements = ${sum}`)
+// Q44 Max Element in array
 
-// Q44 Max Element in array 
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i]
+//     }
+// }
 
-// Q45 Second max element in array 
+// console.log("Max number from array=", max)
+// Q45 Second max element in array
+// self OWN logic method 
+// arr = [46, 57, 1, 2, 3]
+// let max = arr[0];
+// let secondMax = arr[1];
+// if (max < secondMax) {
+//     max = arr[1];
+//     secondMax = arr[0]
+// }else if(arr[i]>secondMax && arr[i] != max){
+//     secondMax = arr[i]
+// }
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         secondMax = max;
+//         max = arr[i]
+//     }
+// }
+// console.log(`Second max is = ${secondMax} and Max = ${max} `)
+
+//Right second method 
+let max = Math.max(arr[0], arr[1]);
+let secondMax = Math.min(arr[0], arr[1]);
+
+for (let i = 2; i < arr.length; i++) {
+    if (arr[i] > max) {
+        secondMax = max;
+        max = arr[i]
+    } else if (arr[i] > secondMax && arr[i] != max) {
+        secondMax = arr[i]
+    }
+}
+console.log(`Second max is = ${secondMax} and Max = ${max} `)
 
 // Q46 Reverse an array
 
